81cb431d112b91f9fa3e0b6bce08df086e3a44d4
arcevit <hakkisuman@gmail.com>
2019-09-27 12:58:18 +0200

    kkkd

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const proxy = require('http-proxy-middleware');
-module.exports = function(app) {
-  app.use(
-    proxy('ws://date.nager.at/Api/v2')
-  );
-};
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+//   app.use(
+//     proxy('ws://date.nager.at/Api/v2')
+//   );
+// };
