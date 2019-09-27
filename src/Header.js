7445dd229c9b432460deffebd6037171236adb6b
arcevit <hakkisuman@gmail.com>
2019-09-27 22:34:39 +0200

    bla

--- src/Header.js
+++ src/Header.js
@@ -8,3 +8,16 @@ export default function Header() {
     </div>
   );
 }
+
+
+// const proxy = require('http-proxy-middleware');
+// module.exports = function(app) {
+//   app.use(
+//     proxy("/AvailableCountries",{
+//       target:"https://date.nager.at/Api/v2",
+//       secure:false,
+//       changeOrigin:true
+      
+//     })
+//   );
+// };
